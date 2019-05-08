20664e83fc8f5abbddcd5f659b90ce4861b8dd6f
Alain Belair <belair.alain@gmail.com>
2019-05-08 16:29:41 -0400

    final

--- src/Model/Invoice.php
+++ src/Model/Invoice.php
@@ -10,140 +10,30 @@ use atk4\ui\Exception;
 class Invoice extends \atk4\data\Model
 {
     public $table = 'invoice';
-    public $tableCaption = 'Invoice';
 
     public $period = '30';
     public $taxRate = 0.1;
-
-
-    public $items = null;
-    public $itemsRef = null;
-    public $itemsRefId = null;
-
-    public $billTo = null;
-    public $billToRef = null;
-
-    public $paiement = null;
-    public $paiementRef = null;
-
-    public $headerFields = null;
-    public $footerFields = null;
 
 
     public function init()
     {
         parent::init();
 
-        if (!$this->items || !$this->itemsRef || !$this->itemsRefId) {
-            throw new Exception('Items, ItemsRef or itemsRefId need to be set.');
-        }
-
         $this->addField('reference', ['required' => true, 'ui' => ['form' => ['width' => 'six']]]);
 
-        $this->addField('date', ['type' => 'date', 'default' => new \DateTime(), 'required' => true/*, 'ui' => ['form' => ['width' => 'four']]*/]);
-        $this->addField('due_date', ['type' => 'date', 'default' => (new \DateTime())->add(new \DateInterval('P'.$this->period.'D'))/*,'ui' => ['form' => ['width' => 'four']]*/]);
+        $this->addField('date', ['type' => 'date', 'default' => new \DateTime(), 'required' => true, 'ui' => ['form' => ['width' => 'four']]]);
+        $this->addField('due_date', ['type' => 'date', 'default' => (new \DateTime())->add(new \DateInterval('P'.$this->period.'D')),'ui' => ['form' => ['width' => 'four']]]);
 
-        if ($this->billTo) {
-            $this->hasOne($this->billToRef, $this->billTo, ['required' => true/*, 'ui' => ['form' => ['width' => 'three']]*/]);
-        }
+        $this->hasOne('bill_to_id', new Client(), ['required' => true, 'ui' => ['form' => ['width' => 'three']]])->addField('client', 'name');
 
-        if ($this->paiement) {
-            $this->hasMany('Paiements', new Paiement())->addField('paid_total', ['aggregate' => 'sum', 'field' => 'amount', 'type' => 'money'/*, 'ui' => ['form' => ['width' => 'three']]*/]);
-        }
+        $this->hasMany('Paiements', new Paiement())->addField('paid_total', ['aggregate' => 'sum', 'field' => 'amount', 'type' => 'money', 'ui' => ['form' => ['width' => 'three']]]);
 
-        $this->hasMany($this->itemsRef, $this->items)->addField('sub_total', ['aggregate'=>'sum', 'field'=>'amount', 'type' => 'money']);
+        $this->hasMany('Items', new InvoiceItems())->addField('sub_total', ['aggregate'=>'sum', 'field'=>'amount', 'type' => 'money']);
 
         $this->addExpression('tax', ['expr' => "[sub_total] * {$this->taxRate}", 'type' => 'money']);
         $this->addExpression('g_total', ['expr' => '[sub_total]+[tax]', 'type' => 'money']);
 
-
         $this->addExpression('balance', ['expr' => '[g_total] - [paid_total]', 'type' => 'money']);
-
-        //$this->headerFields = ['reference', 'date', 'due_date', 'bill_to_id', 'paid_total'];
-        //$this->footerFields = ['sub_total', 'tax', 'g_total'];
-    }
-
-    /**
-     * Return an array of field name that need to be place above the MultiLine.
-     *
-     * @return array
-     */
-    public function getHeaderFields()
-    {
-        return $this->headerFields;
-    }
-
-    /**
-     * Return Items model table caption.
-     *
-     * @return mixed
-     */
-    public function getItemsTableCaption()
-    {
-        return $this->items->tableCaption;
-    }
-
-    /**
-     * Return an array of field name that need to be set in each item row of the MultiLine field.
-     *
-     * @return mixed
-     */
-    public function getItemFields()
-    {
-        return $this->items->getItemFields();
-    }
-
-    /**
-     * Return an array of field name that need to be place below MultiLine field.
-     * @return array
-     */
-    public function getFooterFields()
-    {
-        return $this->footerFields;
-    }
-
-    /**
-     * Return the fields that will trigger Multiline onChange event.
-     *
-     * @return mixed
-     * @throws Exception
-     */
-    public function getEventFields()
-    {
-
-        return $this->items->getEventFields();
-    }
-
-    /**
-     * Return total of each item in a row.
-     *
-     * @param $itemRows
-     *
-     * @return float|int
-     */
-    public function getSubTotal($itemRows)
-    {
-        $s_total = 0;
-        foreach ($itemRows as $row => $cols) {
-            $qty = array_column($cols, 'qty')[0];
-            $rate = array_column($cols, 'rate')[0];
-            $s_total = $s_total + ($qty * $rate);
-        }
-        $this->total = $s_total;
-
-        return $s_total;
-    }
-
-    /**
-     * Return tax amount from total amount.
-     *
-     * @param $total
-     *
-     * @return float|int
-     */
-    public function getTotalTax($total)
-    {
-        return $total * $this->taxRate;
     }
 
     /**
@@ -163,5 +53,37 @@ class Invoice extends \atk4\data\Model
             $f->getField('tax')->jsInput()->val(number_format($tax, 2)),
             $f->getField('g_total')->jsInput()->val(number_format($s_total + $tax, 2))
         ];
+    }
+
+    /**
+     * Return total of each item in a row.
+     *
+     * @param $itemRows
+     *
+     * @return float|int
+     */
+    protected function getSubTotal($itemRows)
+    {
+        $s_total = 0;
+        foreach ($itemRows as $row => $cols) {
+            $qty = array_column($cols, 'qty')[0];
+            $rate = array_column($cols, 'rate')[0];
+            $s_total = $s_total + ($qty * $rate);
+        }
+        $this->total = $s_total;
+
+        return $s_total;
+    }
+
+    /**
+     * Return tax amount from total amount.
+     *
+     * @param $total
+     *
+     * @return float|int
+     */
+    protected function getTotalTax($total)
+    {
+        return $total * $this->taxRate;
     }
 }
