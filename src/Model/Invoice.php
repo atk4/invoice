b3ffd82d729cbb24319245430dbdabac5fedabbc
Alain Belair <belair.alain@gmail.com>
2020-05-11 13:23:52 -0400

    [fix] Pay invoice action

--- src/Model/Invoice.php
+++ src/Model/Invoice.php
@@ -5,6 +5,7 @@
 namespace atk4\invoice\Model;
 
 use atk4\data\Model;
+use atk4\data\UserAction\Generic;
 use atk4\ui\ActionExecutor\UserAction;
 use atk4\ui\Exception;
 use atk4\invoice\Traits\SimpleTax;
