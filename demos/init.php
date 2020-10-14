8d992bcb573ea1d96d0a2069d97bf763dc1fd3c1
Imants Horsts <DarkSide666@users.noreply.github.com>
2020-10-14 23:26:23 +0300

    fix compatibility with 2.3.1 (#23)

--- demos/init.php
+++ demos/init.php
@@ -4,5 +4,7 @@ require_once '../vendor/autoload.php';
 require_once __DIR__ . '/config.php'; 
 
 $app = new \atk4\ui\App();
-$app->initLayout('Centered');
-$app->dbConnect($config['db']);
+$app->initLayout([\atk4\ui\Layout\Centered::class]);
+
+$db = new \atk4\data\Persistence\Sql($config['db']);
+$app->db = $db;
